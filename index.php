414439c3e93f0a00ad9616785dc3fb707c5f9f77
fed-v <fed_v@hotmail.com>
2014-03-14 18:18:39 -0300

    Back to normal

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 	<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
 	<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
 	<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
-    <!-- test-->
+  
 	<head>
 		<!-- declare all page rendering and programmatic related tags -->
 		<meta charset="utf-8"/>
