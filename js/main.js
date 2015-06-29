1a62e0c4c06ed766f29da0fad3a7b61f2aa1bc6f
fED <fed_v@hotmail.com>
2015-06-29 00:59:27 -0400

    Added Glow mixin
    Added an interesting CSS3 Glow effect for text and cleaned up the index
    file a bit.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
 jQuery( document ).ready(function( $ ) {
-
-	//alert("loaded");
-
+	
+	
+	//CREATE INSTANCE OF THE DATE OBJECT	
+	var date = new Date();	
+								
+	//EXTRACT FOUR DIGIT YEAR AND ADD TO SPAN
+	document.getElementById('year').innerHTML = date.getFullYear();
+	
+	
 	$("#myForm").submit(function() {
 		$.post('includes/sendMail.php', {name: $('#name').val(), email: $('#email').val(), message: $('#message').val(), myFormSubmitted: 'yes'}, function(data) {
 			$("#formResponse").html(data).fadeIn('100');
@@ -10,4 +16,5 @@ jQuery( document ).ready(function( $ ) {
 		return false;
 	});
 
+	
 });
